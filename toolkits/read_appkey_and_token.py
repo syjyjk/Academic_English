31a8a82455eaa7304469d274972c2650a4bf04a6
Sun Yu <201421140010@mail.bnu.edu.cn>
2020-07-12 21:57:58 +0800

    If appkey and token file do not exist, create the file.

--- toolkits/read_appkey_and_token.py
+++ toolkits/read_appkey_and_token.py
@@ -1,4 +1,14 @@
+#coding=utf-8
+import os
 def read_appkey():
+  filelist = os.listdir(os.getcwd()) 
+  if 'appkey.txt' in filelist:
+    print('appkey exist')
+  else:
+    with open('appkey.txt','w+') as ff:
+      content = input('Enter Your Appkey:')
+      ff.write(content)
+      
   with open('appkey.txt') as ff:
     info = ff.readlines()
     for item in info:
@@ -7,11 +17,17 @@ def read_appkey():
         return item_t
 
 def read_token():
+  filelist = os.listdir(os.getcwd()) 
+  if 'token.txt' in filelist:
+    print('token exist')
+  else:
+    with open('token.txt','w+') as ff:
+      content = input('Enter Your Token:')
+      ff.write(content)
+ 
   with open('token.txt') as ff:
     info = ff.readlines()
     for item in info:
       item_t = item.strip().strip('\n')
       if item_t != '':
         return item_t
-
-
